c4d3f80f28d83d13c59886d2488f927c30cfd62e
Maksin Spizhuk <indcheat@mail.ru>
2025-06-11 19:38:00 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -2,21 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 
-function Header({ urlImg, title }) {
+function Header(props) {
   return (
-    <div className="imgLogo">
-      <h1>{title}</h1>
-      <img src={urlImg} alt="randomImage"></img>
+    <div className="container">
+      <h1>{props.nameSite}</h1>
+      <header className="imgLogo">
+        <img src={props.urlImg} alt="randomImage" />
+      </header>
+      <hr></hr>
+      <nav>
+        <ol>
+          <li>{props.nav.title}</li>
+          <li>{props.nav.menu}</li>
+          <li>{props.nav.faq}</li>
+          <li>{props.nav.date}</li>
+          <li>{props.nav.about}</li>
+        </ol>
+      </nav>
+      <hr></hr>
+      <footer>@copyright все права защищены {props.footer} год.</footer>
     </div>
   );
 }
 
-function Nav() {}
+const navData = {
+  title: "Новости",
+  menu: "Menu",
+  faq: "FAQ",
+  date: new Date().toString(),
+  about: "About us",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Header
     urlImg="https://i.pinimg.com/originals/ae/9b/e1/ae9be178fac17c37c1ef47e1a0c06241.jpg"
-    title="DevTools"
+    nameSite="DevTools"
+    nav={navData}
+    footer={new Date().getFullYear()}
   />
 );
